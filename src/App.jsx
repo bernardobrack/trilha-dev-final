a7a4aae7ce47679b4c5ebc0778c6961425341ca5
Bernardo Brack <be.gbrack@gmail.com>
2022-12-29 15:11:56 -0300

    adds styles to texts and Button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import { useState } from 'react'
+import Button from './components/Button/Button'
 import './App.css'
 
 function App() {
 
   return (
     <div className="App">
-      
+      <h1>Display 1</h1>
+      <h2>Display 2</h2>
+      <h3>Display 3</h3>
+      <h4>Display 4</h4>
+      <h5>Display 5</h5>
+      <h6>Display 6</h6>
+      <p>Paragraph</p>
     </div>
   )
 }
---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,9 @@
+import "./Button.css";
+
+export default function Button(props) {
+    const {className, children, ...rest} = props;
+
+    return <button 
+                className={`btn ${className}`} 
+                {...rest}>{children}</button>
+}
