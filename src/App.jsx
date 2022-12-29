3c7e804b12c3fb2f272148d96db0e5290ae325cb
Bernardo Brack <be.gbrack@gmail.com>
2022-12-29 15:17:16 -0300

    adds style to Buttons

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,9 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Display 1</h1>
-      <h2>Display 2</h2>
-      <h3>Display 3</h3>
-      <h4>Display 4</h4>
-      <h5>Display 5</h5>
-      <h6>Display 6</h6>
-      <p>Paragraph</p>
+      <Button className="btn-large">Botao Large</Button>
+      <Button className="btn-medium">Botao Medium</Button>
+      <Button className="btn-small">Botao Small</Button>
     </div>
   )
 }
