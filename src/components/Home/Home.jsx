331a5df990f0d4cf264d044d33d4c4d0cde5da8a
Bernardo Brack <be.gbrack@gmail.com>
2022-12-31 17:22:29 -0300

    fix homepage issues

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,7 +4,6 @@ import { useFetch } from "../../hooks/useFetch";
 import Header from "../Header/Header";
 import "./Home.css";
 import QuizCard from "../QuizCard/QuizCard";
-
 export default function Home() {
     
     const { user } = useContext(AppContext);
