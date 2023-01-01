8a7ea322a9c604c3d863052778b22faa4745f83e
Bernardo Brack <be.gbrack@gmail.com>
2023-01-01 19:54:34 -0300

    add Loader component to HomeContent

--- src/components/HomeContent/HomeContent.jsx
+++ src/components/HomeContent/HomeContent.jsx
@@ -4,14 +4,16 @@ import { fetcher } from "../../hooks/helper";
 import useSWR from "swr";
 import { useSearchParams } from "react-router-dom";
 import QuizNotFound from "../QuizNotFound/QuizNotFound";
+import Loader from "../Loader/Loader";
 
 export default function HomeContent() {
     const [params, setParams] = useSearchParams();
-    const { data, error, isLoading } = useSWR( params.get("search") ? `quizzes?search=${params.get("search")}` : 'quizzes', fetcher);
+    const { data, error, isLoading, isMutating } = useSWR( params.get("search") ? `quizzes?search=${params.get("search")}` : 'quizzes', fetcher);
     return <div id="home-page-content">
         {data && data.length>0 && <section className="grid" id="home-page-grid">
             {data.map(quiz => <QuizCard key={quiz.id} quizInfo={quiz}/>)}
         </section>}
+        {!data && <Loader/>}
         {(data?.length<=0) && <QuizNotFound />}
     </div>
 }
---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,7 @@
+import "./Loader.css";
+
+export default function Loader() {
+    return <div id="loader-div">
+        <div className="lds-ring"><div></div><div></div><div></div><div></div></div>
+    </div> 
+}
