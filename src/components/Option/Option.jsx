45431b7af5585cbafc50c1bd14fedacdb592f00e
Bernardo Brack <be.gbrack@gmail.com>
2023-01-02 16:59:27 -0300

    fix identation in Option component

--- src/components/Option/Option.jsx
+++ src/components/Option/Option.jsx
@@ -2,7 +2,7 @@ import "./Option.css"
 
 export default function Option(props) {
     const {showType, correct, letter, text, onClick, index, disabled} = props;
-    const classes = `${correct ? "correct" : "wrong"} ${disabled ? "disabled" : ""}` ;
+    const classes = `${correct ? "correct" : "wrong"} ${disabled ? "disabled" : ""}`;
     return <div onClick={() => onClick(index)} className={`option-div ${showType ? `${classes}` : `${disabled ? "disabled" : ""}`}`}>
         <div className="option-content">
             <p className="p-large">{letter}.</p>
